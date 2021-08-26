67557b01b356a4fd043049099b8af9d791293cf1
Amanda Cleto <amanda.clet@gmail.com>
2021-08-26 17:29:32 -0300

    added reflective object by the other objects

--- src/index.js
+++ src/index.js
@@ -15,22 +15,25 @@ import {
 	DoubleSide,
 	MeshBasicMaterial,
 	PointLight,
-	TorusGeometry
+	TorusGeometry,
+	WebGLCubeRenderTarget,
 } from 'three';
+
+import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-let scene, camera, renderer, cylinder, sphere, building;
+let scene, camera, renderer, cylinder, sphere, building, cubeCamera, luminaire;
 
 init();
 
 function init(){
 	//create scene
 	scene = new Scene();
-	scene.background = new Color(0x000000);
+	scene.background = new Color(0xffffff);
 
 	//create camera
 	camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-	camera.position.z = 2;
+	camera.position.set(40, 20, 20);
 
 	//create renderer
 	renderer = new WebGLRenderer();
@@ -40,8 +43,8 @@ function init(){
 
 	//create controls
 	const controls = new OrbitControls( camera, renderer.domElement );
-	controls.target.set(-20,-20,10);
-	controls.minDistance = 80;
+	controls.target.set(0, 0, 0);
+	controls.minDistance = 0;
 	controls.maxDistance = 100;
 	controls.update();
 
@@ -56,12 +59,27 @@ function init(){
 	ambientLight.position.set(0, 0, 0)
 	scene.add(ambientLight);
 
-	//create objects
+	//ground
+	const heightGround = 5;
+	const widthGround = 100;
+	const groundGeometry = new BoxGeometry( widthGround, heightGround, widthGround );
+	const groundMaterial = new MeshBasicMaterial( {color: '#252525', side: DoubleSide} );
+	const ground = new Mesh( groundGeometry, groundMaterial );
+	ground.rotation.y = -(Math.PI / 2);
+	ground.position.y -= heightGround / 2;
 
+	scene.add( ground );
+
+
+
+
+
+
+	//create objects
 	//group cylinder and sphere into one mesh
 	const capsulePerson = new Group();
 
-	//cylinder
+	//cylinder body
 	const personCylinderHeight = 5;
 	const personCylinderGeometry = new CylinderGeometry(0.005, 2, personCylinderHeight, 10, 1);
 	const personCylinderMaterial = new MeshStandardMaterial({color:new Color('#cc113c')});
@@ -69,14 +87,14 @@ function init(){
 	cylinder.position.set(20, (personCylinderHeight / 2), -0.5);
 	capsulePerson.add(cylinder);
 
-	//sphere
+	//sphere head
 	const heightHeadSphereGeometry = 40;
 	const headSphereGeometry = new SphereGeometry(1, 10, heightHeadSphereGeometry, 0, 8, 0);
 	const headSphereMaterial = new MeshStandardMaterial({color:new Color('#c2253c')});
 	sphere = new Mesh(headSphereGeometry, headSphereMaterial);
 	sphere.position.set(20, (personCylinderHeight + 0.3), -0.5)
-	capsulePerson.add(sphere);
 
+	capsulePerson.add(sphere);
 	scene.add(capsulePerson);
 
 	//buiding
@@ -104,27 +122,16 @@ function init(){
 		}
 	}
 
-	//ground
-	const heightGround = 5;
-	const widthGround = 100;
-	const groundGeometry = new BoxGeometry( widthGround, heightGround, widthGround );
-	const groundMaterial = new MeshBasicMaterial( {color: '#252525', side: DoubleSide} );
-	const ground = new Mesh( groundGeometry, groundMaterial );
-	ground.rotation.y = -(Math.PI / 2);
-	ground.position.y -= heightGround / 2;
-
-	scene.add( ground );
-
 	//lamp-post
 	//base-lamp-post
 	const capsuleLampPost = new Group();
 
 	const heightBaseLampPost = 2;
 	const baseLampPostGeometry = new CylinderGeometry( 0.5, 1.5, heightBaseLampPost, 6, 1 );
-	const baseLampPostMaterial = new MeshBasicMaterial( {color: '#ccc'} );
-	const baseLampPost = new Mesh( baseLampPostGeometry, baseLampPostMaterial );
-	baseLampPost.position.set(15, (heightBaseLampPost / 2), -8);
-	capsuleLampPost.add(baseLampPost);
+	const baseLampPostMaterial = new MeshBasicMaterial( {color: '#cccccc', side: DoubleSide} ); luminaire
+	const basePost = new Mesh( baseLampPostGeometry, baseLampPostMaterial );
+	basePost.position.set(15, (heightBaseLampPost / 2), -8);
+	capsuleLampPost.add(basePost);
 
 	//trunk-lamp-post
 	const heightTrunkLampPost = 25;
@@ -134,13 +141,18 @@ function init(){
 	baseTrunkPost.position.set(15, (heightTrunkLampPost / 2), -8);
 	capsuleLampPost.add(baseTrunkPost);
 
-	//top-base-post
+	//top-base-post //REFLECT
 	const heightTopBasePost = 1;
 	const topBasePostGeometry = new CylinderGeometry( 0.2, 1, heightTopBasePost, 8, 1, true );
-	const topBasePostMaterial = new MeshBasicMaterial( {color: '#cccccc', side: DoubleSide} );
-	const topBasePost = new Mesh( topBasePostGeometry, topBasePostMaterial );
-	topBasePost.position.set(17,  24.7 + (heightTopBasePost / 2), -5.5);
-	capsuleLampPost.add(topBasePost);
+
+		const renderReflective = new WebGLCubeRenderTarget( 128, { format:  THREE.RGBFormat, generateMipmaps: true, minFilter: THREE.LinearMipmapLinearFilter } );
+		cubeCamera = new THREE.CubeCamera( 1, 100000, renderReflective );
+		scene.add( cubeCamera );
+
+		const luminairePostMaterial = new MeshBasicMaterial({envMap: cubeCamera.renderTarget.texture });
+	luminaire = new Mesh( topBasePostGeometry, luminairePostMaterial );
+		luminaire.position.set(17,  24.7 + (heightTopBasePost / 2), -5.5);
+	capsuleLampPost.add(luminaire);
 
 	//top-base-circle-post
 	const heightTopBaseSpherePost = 0.3;
@@ -163,11 +175,6 @@ function init(){
 	pointLight.position.set(17,  24.7 + (heightLampPost / 2), -5.5);
 	capsuleLampPost.add(pointLight);
 
-	//helper
-	// const sphereSize = 1;
-	// const pointLightHelper = new PointLightHelper( pointLight, sphereSize );
-	// capsuleLampPost.add( pointLightHelper );
-
 	//torus
 	const torusGeometry = new TorusGeometry( 1, .3, 6, 7, 1.6);
 	const torusMaterial = new MeshBasicMaterial( { color: 0xcccccc } );
@@ -176,6 +183,7 @@ function init(){
 	torus.rotation.y = 2.3;
 	capsuleLampPost.add(torus);
 
+	//torus connection
 	const torusLampConnectionGeometry = new CylinderGeometry( 0.3, 0.3, 5, 32);
 	const torusLampConnectionMaterial = new MeshBasicMaterial( { color: 0xcccccc } );
 	const torusConnection = new Mesh( torusLampConnectionGeometry, torusLampConnectionMaterial );
@@ -192,6 +200,10 @@ function init(){
 function update(){
 	requestAnimationFrame( update );
 	cylinder.rotation.y -= 0.01;
+
+	luminaire.visible = false;
+	cubeCamera.update( renderer, scene );
+	luminaire.visible = true;
 
 	renderer.render( scene, camera );
 }
